f0240f7385e5ea8c4b8b6636ca9c2662114bda72
7rans <transfire@gmail.com>
2011-10-08 15:33:21 -0400

    Get VERSION and other metadata for .ruby file.

--- lib/tapout/version.rb
+++ lib/tapout/version.rb
@@ -1,7 +1,20 @@
 module TapOut
-  # Current version of TAPOUT project.
-  VERSION = "0.2.3"
-
   # The current revision of the TAP-Y/J format.
   REVISION = "3"
+
+  # Project metadata.
+  #
+  # @return [Hash] metadata from .ruby file
+  def self.metadata
+    @metadata ||= (
+      require 'yaml'
+      YAML.load_file(File.join(File.dirname(__FILE__), 'tapout.yml'))
+    )
+  end
+
+  # Any missing constant will be looked for in 
+  # project metadata.
+  def self.const_missing(name)
+    metadata[name.to_s.downcase] || super(name)
+  end
 end
